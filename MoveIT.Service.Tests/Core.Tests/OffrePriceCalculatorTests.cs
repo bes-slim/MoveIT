30fcd54b9d1305e02477bb6734827eb333297453
Bessem Slim <slim.bessem@gmail.com>
2015-01-12 14:52:20 +0100

    add mock verify

--- MoveIT.Service.Tests/Core.Tests/OffrePriceCalculatorTests.cs
+++ MoveIT.Service.Tests/Core.Tests/OffrePriceCalculatorTests.cs
@@ -8,23 +8,41 @@ namespace MoveIT.Service.Tests.Core.Tests
 {
     public class OffrePriceCalculatorTests
     {
-        [Fact]
-        public void shoudl_return_9000_excl_vat_5_km_distance_area_of_73_sm_basement_9_sm_and_has_piano()
+        private readonly Mock<IOffreCostsRule> _carsCostRule;
+        private readonly Mock<IOffreCostsRule> _specialFurnitureCostRule;
+        private readonly MoveInfo _info;
+        private readonly OffrePriceCalcualtor _sut;
+        private readonly decimal _price;
+
+        public OffrePriceCalculatorTests()
         {
-            // Arrange
-            var info = new MoveInfo() { Distance = 5, Area = 73, BasementArea = 9, HasPiano = true };
-            var carsCostRule = new Mock<IOffreCostsRule>();
-            var sepecialFurnitureCostRule = new Mock<IOffreCostsRule>();
-
-            carsCostRule.Setup(x => x.CalculatePrice(info)).Returns(2200);
-            sepecialFurnitureCostRule.Setup(x => x.CalculatePrice(info)).Returns(5000);
-
-            var sut = new OffrePriceCalcualtor(new []{carsCostRule.Object, sepecialFurnitureCostRule.Object});
-
-            // Assert
-            Assert.Equal(9000, sut.CalculatePrice(info));
+            _carsCostRule = new Mock<IOffreCostsRule>();
+            _specialFurnitureCostRule = new Mock<IOffreCostsRule>();
+            _carsCostRule.Setup(x => x.CalculatePrice(_info)).Returns(2200);
+            _specialFurnitureCostRule.Setup(x => x.CalculatePrice(_info)).Returns(5000);
+            _info = new MoveInfo() { Distance = 5, Area = 73, BasementArea = 9, HasPiano = true };
+            _sut = new OffrePriceCalcualtor(new[] { _carsCostRule.Object, _specialFurnitureCostRule.Object });
+            _price = _sut.CalculatePrice(_info);
         }
 
 
+        [Fact]
+        public void shoudl_return_9000_excl_vat_5_km_distance_area_of_73_sm_basement_9_sm_and_has_piano()
+        {
+            // Assert
+            Assert.Equal(9000, _price);
+        }
+
+        [Fact]
+        public void should_run_calculate_price_on_cars_rule()
+        {
+            _carsCostRule.Verify(x => x.CalculatePrice(It.IsAny<MoveInfo>()), Times.Once);
+        }
+
+        [Fact]
+        public void should_run_calculate_price_on_special_furniture_rule()
+        {
+            _specialFurnitureCostRule.Verify(x => x.CalculatePrice(It.IsAny<MoveInfo>()), Times.Once);
+        }
     }
 }
